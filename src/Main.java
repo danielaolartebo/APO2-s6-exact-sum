43f4de73f83932cfa7805e4c3f8c46d21f1f7681
Daniela Olarte <danielaolarte123@gmail.com>
2021-03-18 07:24:30 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -10,31 +10,38 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
         PrintWriter pw = new PrintWriter(System.out);
         
-        while (sc.hasNext()) {       	
+        while (sc.hasNext()) {     
+        	
             int N = sc.nextInt(); //number of books
             int[] P = new int[N];
-            
             for (int i = 0; i < N; i++) {
             	 P[i] = sc.nextInt(); //price of each book
+            	 
             }
-            Arrays.sort(P);
+            Arrays.sort(P);     
             
             int M = sc.nextInt(); //amount of money Peter has
             int x = -1;
-            int y = -1;
+            int y = -1;  
             
             for (int i = 0; i < N; i++) {
+            	
                 int T = M - P[i];
                 int Q = Arrays.binarySearch(P,i+1,N,T);
                 
-                if (Q >= 0 && (P[Q] - P[i] < y - x || (y == -1 && x == -1))) {
-                    x = P[i];
-                    y = P[Q];                    
+                if (0 <= Q) {
+                	if (P[Q] - P[i] < y - x || (y == -1 && x == -1)) {
+                    	
+                        x = P[i];
+                        y = P[Q];                    
+                    }
                 }
             }
+            
             pw.println("Peter should buy books whose prices are " +x+ " and "+y+".\n");
             pw.flush();
         }
+        
         pw.close();
     }
 }
